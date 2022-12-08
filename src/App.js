90dd743546102c87b37c828d062d21d41dcb3a66
DidarBhtv <didar680@yandex.com>
2022-12-08 15:50:40 +0100

    not fini

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Success } from './componets/Success';
+import User from './componets/Users/User';
+import './index.css';
+
+
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <User/>
+      <Success />
+    </div>  
   );
-}
+}    
+
 
 export default App;
